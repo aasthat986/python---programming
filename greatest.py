42605d5657f633f6fd7205f4b1767a62ba46fb5a
Aastha Tripathi <aasthatripathi238@gmail.com>
2026-06-10 11:58:47 +0530

    greatest number

---
+++ greatest.py
@@ -0,0 +1,10 @@
+a =int(input("enter a first number"))
+b=int(input("enter a second number"))
+c=int(input("enter a third number"))
+
+if a>b:
+    print("a is greatest")
+    if b>c:
+     print("b is greatest")
+elif c>a:
+  print("c is greatest")
